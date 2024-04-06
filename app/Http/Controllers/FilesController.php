39c3b276754de44d9022671baa32638a7ac26cdd
Vinícius de Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-04-06 18:41:34 -0300

    files controller

---
+++ app/Http/Controllers/FilesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FilesController extends Controller
+{
+    public function salvarArquivo($request)
+    {
+        dd($request);
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,9 +19,11 @@
             </div>
         </div>
 
-        <form action="/target" class="dropzone"></form>
+        <form action="/file-upload" class="dropzone" id="my-awesome-dropzone"></form>
     </div>    
+    <script>
 
+    </script>
     <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
