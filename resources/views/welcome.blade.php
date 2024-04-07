96bb89168d0e924408b715ed2dd180af471d9e43
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-04-07 10:53:46 -0300

    jquery

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 </head>
 <body>
+    <h1>Teste</h1>
+    <button id="button">TESTE</button>
 
+    <script>
+        $(document).ready(function(){
+            $("#button").click(function(){
+                alert("Você clicou no botão!");
+            });
+        });
+    </script>
 </body>
 </html>
