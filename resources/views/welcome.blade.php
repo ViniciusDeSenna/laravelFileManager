6ceecfc05d71affc2f9c03d7f9c6b27640b5e356
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-04-07 18:51:24 -0300

    dropzone indo para o controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,53 +6,59 @@
     <title>Formulário com AJAX</title>
     <!-- Adicionando Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
 </head>
 <body>
 
 <div class="container mt-5">
     <div class="row">
         <div class="col-md-6 offset-md-3">
-            <h2>Formulário com AJAX</h2>
-            <form id="meuFormulario">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <div class="form-group">
-                    <label for="nome">Nome:</label>
-                    <input type="text" class="form-control" id="nome" name="nome">
-                </div>
-                <div class="form-group">
-                    <label for="email">E-mail:</label>
-                    <input type="email" class="form-control" id="email" name="email">
-                </div>
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>
-            <div id="mensagem"></div>
+            <h2 class="text-center mb-4">Enviar arquivo</h2>
+
+            <form action="{{route('files.upload')}}" class="dropzone" id="myDropzone">@csrf</form>
         </div>
     </div>
 </div>
+
 
 <!-- Adicionando jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <!-- Adicionando Bootstrap JS -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script>
-    $(document).ready(function(){
-        $('#meuFormulario').submit(function(e){
-            e.preventDefault();
-            var formData = $(this).serialize();
-            $.ajax({
-                type: 'POST',
-                url: '{{route('files.upload')}}',
-                data: formData,
-                headers: {'X-CSRF-TOKEN': '{{csrf_token()}}'},
-                success: function(data){
-                    console.log(data)
-                },
-                error: function(data){
-                    console.log(data)
-                }
-            });
-        });
-    });
+    Dropzone.options.myDropzone = {
+        paramName: "file",
+        maxFilesize: 2,
+        maxFiles: 5,
+        acceptedFiles: '.jpg, .jpeg, .png, .pdf',
+        success: function(file, response) {
+            console.log(response);
+        },
+        error: function(file, response) {
+            console.log(response);
+        }
+    };
+
+    {{--function enviar()--}}
+    {{--{--}}
+    {{--    let formData = new FormData();--}}
+    {{--    let fileInput = $('#file-input')[0];--}}
+    {{--    formData.append('file', fileInput);--}}
+    {{--    console.log(formData)--}}
+    {{--    $.ajax({--}}
+    {{--        type: 'POST',--}}
+    {{--        url: '{{route('files.upload')}}',--}}
+    {{--        data: formData,--}}
+    {{--        headers: {'X-CSRF-TOKEN': '{{csrf_token()}}'},--}}
+    {{--        success: function(data){--}}
+    {{--            console.log(data)--}}
+    {{--        },--}}
+    {{--        error: function(data){--}}
+    {{--            console.log(data)--}}
+    {{--        }--}}
+    {{--    });--}}
+    {{--}--}}
 </script>
 </body>
 </html>
