b823679cb7c8bab93052f732d57fd8a822228296
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-04-07 11:48:25 -0300

    dropzone instalado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,16 +14,11 @@
 <body>
 
 <div class="container">
-    <h1>Teste</h1>
-    <button id="button" class="btn btn-success">TESTE</button>
-    <form action="/target" class="dropzone"></form>
+    <form action="/file-upload" class="dropzone" id="my-dropzone"></form>
 </div>
 
 <script>
-    function upAjax()
-    {
-        
-    }
+
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
