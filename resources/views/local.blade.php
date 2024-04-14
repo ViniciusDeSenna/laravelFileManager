bcaec38657a5eea6b998cb67986c504f6f7afba6
Vinícius Senna <vinicius.senna@csxsolucoes.com.br>
2024-04-14 12:41:47 -0300

    ajustes

--- resources/views/local.blade.php
+++ resources/views/local.blade.php
@@ -1,5 +1,46 @@
-@extends('layout.dashboard.dashboard')
+@extends('layout.dashboard')
 
 @section('content')
-<h1>teste</h1>
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <h2 class="text-center mb-4">Enviar arquivo</h2>
+                <input type="hidden" value="local/" id="fileLocal">
+                <form action="{{route('files.upload')}}" class="dropzone" id="myDropzone">@csrf</form>
+            </div>
+        </div>
+    @if(isset($files))
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <h2 class="text-center mb-4">Arquivos Disponíveis</h2>
+                    <ul class="list-group">
+                        @foreach($files as $file)
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <a href="#">{{$file->name}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    @endif
+
+    <script>
+        Dropzone.options.myDropzone = {
+            paramName: "file",
+            maxFilesize: 2,
+            maxFiles: 5,
+            acceptedFiles: '.jpg, .jpeg, .png, .pdf',
+            sending: function(file, xhr, formData) {
+                let fileLocal = $('#fileLocal').val();
+                formData.append("fileLocal", fileLocal);
+            },
+            success: function(file, data) {
+                console.log(data)
+            },
+            error: function(data) {
+                alert(data)
+            }
+        };
+    </script>
 @endsection
