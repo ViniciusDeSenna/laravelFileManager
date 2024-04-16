bef9bffaa472e7df4a712b1a88839b14ff6a3427
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2024-04-16 00:29:25 -0300

    Pastas

--- resources/views/local.blade.php
+++ resources/views/local.blade.php
@@ -1,30 +1,33 @@
 @extends('layout.dashboard')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-6 offset-md-3">
-            <h2 class="text-center mb-4">Enviar arquivo</h2>
-            <input type="hidden" value="{{$folder->parent_folder_id}}" id="fileLocal">
-            <form action="{{route('files.upload')}}" method="POST" class="dropzone" id="myDropzone">@csrf</form>
+    <div class="container-fluid">
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">@if($folder->name == 'local')My Storage @else {{$folder->name}} @endif</h1>
+            <a class="btn btn-primary btn-icon-split" onclick="newFolder()">
+                <span class="icon text-white-50"><i class="fa fa-folder-plus"></i></span>
+                <span class="text">New Folder</span>
+            </a>
         </div>
-    </div>
-    @if(isset($folders))
-        <div class="container mt-5">
+        <!-- Folders -->
+        @if(isset($folders))
             <div class="row">
-                <div class="col-md-6 offset-md-3">
-                    <h2 class="text-center mb-4">Pastas</h2>
-                    <ul class="list-group">
-                        @foreach($folders as $folder)
-                            <li class="list-group-item d-flex justify-content-between align-items-center">
-                                <a href="{{route('folder.view', [$folder->name])}}">{{$folder->name}}</a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
+                @foreach($folders as $folder)
+                    <div class="col-4">
+                        <a class="text-decoration-none" href="{{route('folder.view', [$folder->name])}}">
+                            <div class="card mb-4 border-bottom-primary">
+                                <div class="card-body">
+                                    <span class="icon text-blue-50"><i class="fa fa-folder-open"></i></span>
+                                    {{$folder->name}}
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                @endforeach
             </div>
-            <button type="button" class="btn btn-primary" onclick="newFolder()">New Folder</button>
-        </div>
-    @endif
+        @endif
+    </div>
     @if(isset($files))
         <div class="container mt-5">
             <div class="row">
@@ -41,6 +44,14 @@
             </div>
         </div>
     @endif
+
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <h2 class="text-center mb-4">Enviar arquivo</h2>
+            <input type="hidden" value="{{$folder->parent_folder_id}}" id="fileLocal">
+            <form action="{{route('files.upload')}}" method="POST" class="dropzone" id="myDropzone">@csrf</form>
+        </div>
+    </div>
 
     <script>
         Dropzone.options.myDropzone = {
